7d42d4ee7059fcfd87a376e24986ab9ea488ad6a
Alfre <fred.morieri@gmail.com>
2022-03-02 23:29:47 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -13,21 +13,29 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
-
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. Te loop will repeatedly request data, until it is valid.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            # This check only validates data. Conversions of data inside,
+            # although useful, are not returned. So, math must be done 
+            # with newly converted data.
+            print('Data is valid!')
+            break
+    return sales_data
 
 
 def validate_data(values):
@@ -37,10 +45,8 @@ def validate_data(values):
     or if there aren't exactly  values.
     """
     [int(value) for value in values]
-    """
-    list comprehension: for each individual value in the 
-    values  list, convert that value into an integer
-    """
+    # list comprehension: for each individual value in the
+    # values  list, convert that value into an integer
     try:
         if len(values) != 6:
             raise ValueError(
@@ -48,6 +54,23 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
+        return False
+
+    return True
 
 
-get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet('sales')
+    # line above gets access to worksheet name 'sales' inside the workbook
+    sales_worksheet.append_row(data)
+    # line above adds a new row in the selected worksheet with data passed in
+    print('Sales worksheet updated successfully.\n')
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
