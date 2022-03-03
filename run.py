ab55d99eec0ca3bbbf2cffa8fbf03e4484de6441
Alfre <fred.morieri@gmail.com>
2022-03-03 23:27:01 +0000

    Solved final challenge

--- run.py
+++ run.py
@@ -156,7 +156,23 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, 'stock')
+    return stock_data
 
 
 print('Welcome to Love Sandwiches Data Automation')
-main()
+stock_data = main()
+
+
+def get_stock_values(data):
+    """
+    This function is part of CI final challenge and is supposed
+    to return a dict of headings and values
+    """
+    headings = SHEET.worksheet('stock').get_all_values()[0]
+    stock_dict = {headings[i]:data[i] for i in range(6)}
+
+    return stock_dict
+   
+
+stock_values = get_stock_values(stock_data)
+print(stock_values)
